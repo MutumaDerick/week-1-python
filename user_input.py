0b2026c30cacd650dcc836e5c219dadd54d10868
MutumaDerick <pdmutuma@gmail.com>
2024-08-14 13:21:19 +0300

    User details capture

---
+++ user_input.py
@@ -0,0 +1,7 @@
+#Ask the user for their name, age and location
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where are you from? ")
+
+#Print out the user's name, age and location
+print(f"Hello {name}, you are {age} years old and you are from {location}.")
